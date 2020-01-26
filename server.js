a5c3d4a740cc5709be23a92e68ecb7e8e2b9dc8b
Eric Crescioni <ecam900@gmail.com>
2020-01-26 19:12:23 -0400

    Add mongoDB, mongoose, colors

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-
-// Route files
-const bootcamps = require('./routes/bootcamps');
+const colors = require('colors');
+const connectDB = require('./config/db');
 
 // Load env vars
 dotenv.config({
   path: './config/config.env'
 });
+
+// Connect to Database
+connectDB();
+
+// Route files
+const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
@@ -22,7 +27,16 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
+
+// Handle unhnalded promise rejections (Doesn't work on Windows. Will switch to Try-Catch later.)
+// process.on('unhandledRejection', (err, promise) => {
+//   console.log(`Error: ${err.message}`)
+//   // Close Server & exit process
+//   server.close(() => process.exit(1));
+// });
