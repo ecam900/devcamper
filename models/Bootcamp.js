6e024ddc6c48901aff9fa69444b03fcdc66b044f
Eric Crescioni <ecam900@gmail.com>
2020-02-08 13:06:52 -0400

    Add slugify to create slugs. Start adding geocoder.

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const BootcampSchema = new mongoose.Schema({
   name: {
@@ -96,8 +97,16 @@ const BootcampSchema = new mongoose.Schema({
   createdAt: {
     type: Date,
     default: Date.now
-  },
+  }
+});
 
+// Create bootcamp slug from the name
+// >> Cannot use arrow function here due to Scope. Don't refactor.
+BootcampSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, {
+    lower: true
+  })
+  next();
 });
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema);
