6b6938f57924d85913178a3c450106974d47cc96
Eric Crescioni <ecam900@gmail.com>
2020-02-08 18:29:27 -0400

    Add db Seeder.

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -123,6 +123,9 @@ BootcampSchema.pre('save', async function (next) {
     zipcode: loc[0].zipcode,
     country: loc[0].countryCode,
   }
+
+  // Do not save address in DB
+  this.address = undefined;
   next();
 });
 
