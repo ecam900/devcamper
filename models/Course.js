4e9223d0607473d9be7e46641bacff5b743b65d8
Eric Crescioni <ecam900@gmail.com>
2020-03-19 12:28:32 -0400

    Create Course model.

---
+++ models/Course.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const CourseSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    trim: true,
+    required: [true, 'Please add a course title']
+  },
+  description: {
+    type: String,
+    required: [true, 'Please add a course description']
+  },
+  weeks: {
+    type: String,
+    required: [true, 'Please add number of weeks']
+  },
+  tuition: {
+    type: Number,
+    required: [true, 'Please add tuition cost']
+  },
+  minimumSkill: {
+    type: String,
+    required: [true, 'Please add minimum skill'],
+    enum: ['beginner', 'intermediate', 'advanced']
+  },
+  scholarshipAvailable: {
+    type: Boolean,
+    default: false
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+
+  // This is added because Courses are related to Bootcamps. Every course has to have a bootcamp.
+  bootcamp: {
+    type: moongose.Schema.ObjectId,
+    ref: 'Bootcamp',
+    required: true
+  }
+});
+
+module.exports = mongoose.model('Course', CourseSchema);
