22a6b2a0c38579cf100f8d366bb7afbdb81437d8
Eric Crescioni <ecam900@gmail.com>
2020-03-20 16:24:59 -0400

    Implement mongoose pre-middleware for cascading delete.

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -139,11 +139,14 @@ exports.updateBootcamp = asyncHandler(async (req, res, next) => {
 // @access  Private
 exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
 
-  const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+  const bootcamp = await Bootcamp.findById(req.params.id);
 
   if (!bootcamp) {
     return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
   }
+
+  // Use this instead of findByIdAndDelete because the latter wont work with our cascading deletion mongoose pre-middleware.
+  bootcamp.remove();
 
   res.status(200).json({
     sucess: true,
