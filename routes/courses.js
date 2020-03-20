85c965d849d278a1d7648010efb188aeac9cb83b
Eric Crescioni <ecam900@gmail.com>
2020-03-20 17:20:04 -0400

    Add create course route.

--- routes/courses.js
+++ routes/courses.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const {
   getCourses,
-  getCourse
+  getCourse,
+  addCourse
 } = require('../controllers/courses');
 
 const router = express.Router({
   mergeParams: true
 });
 
-router.route('/').get(getCourses);
+router.route('/').get(getCourses).post(addCourse);
 router.route('/:id').get(getCourse);
 
 module.exports = router;
