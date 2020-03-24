c960dca0072d9598586c74d6b2c93f3da197c5ff
Eric Crescioni <ecam900@gmail.com>
2020-03-23 21:30:51 -0400

    Refactor advanced results into middleware. Implement advanced results for Courses.

--- controllers/courses.js
+++ controllers/courses.js
@@ -3,32 +3,24 @@ const asyncHandler = require('../middleware/async');
 const Course = require('../models/Course');
 const Bootcamp = require('../models/Bootcamp');
 
-
 // @desc    Get courses
 // @route   GET /api/v1/courses
 // @route   GET /api/v1/bootcamps/:bootcampId/courses
 // @access  Public
 exports.getCourses = asyncHandler(async (req, res, next) => {
-  let query;
-
   if (req.params.bootcampId) {
-    query = Course.find({
+    const courses = await Course.find({
       bootcamp: req.params.bootcampId
     });
-  } else {
-    query = Course.find().populate({
-      path: 'bootcamp',
-      select: 'name description'
+
+    return res.status(200).json({
+      success: true,
+      count: courses.length,
+      data: courses
     });
+  } else {
+    res.status(200).json(res.advancedResults);
   }
-
-  const courses = await query;
-
-  res.status(200).json({
-    success: true,
-    count: courses.length,
-    data: courses
-  });
 });
 
 // @desc    Get single course
@@ -41,7 +33,10 @@ exports.getCourse = asyncHandler(async (req, res, next) => {
   });
 
   if (!course) {
-    return next(new ErrorResponse(`No course with the id of ${req.params.id}`), 404)
+    return next(
+      new ErrorResponse(`No course with the id of ${req.params.id}`),
+      404
+    );
   }
 
   res.status(200).json({
@@ -59,7 +54,10 @@ exports.addCourse = asyncHandler(async (req, res, next) => {
   const bootcamp = await Bootcamp.findById(req.params.bootcampId);
 
   if (!bootcamp) {
-    return next(new ErrorResponse(`No bootcamp with the id of ${req.params.id}`), 404)
+    return next(
+      new ErrorResponse(`No bootcamp with the id of ${req.params.id}`),
+      404
+    );
   }
 
   const course = await Course.create(req.body);
@@ -74,17 +72,19 @@ exports.addCourse = asyncHandler(async (req, res, next) => {
 // @route   PUT /api/v1/courses/:id
 // @access  Private
 exports.updateCourse = asyncHandler(async (req, res, next) => {
-
   let course = await Course.findById(req.params.id);
 
   if (!course) {
-    return next(new ErrorResponse(`No course with the id of ${req.params.id}`), 404)
+    return next(
+      new ErrorResponse(`No course with the id of ${req.params.id}`),
+      404
+    );
   }
 
   course = await Course.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
     runValidators: true
-  })
+  });
 
   res.status(200).json({
     success: true,
@@ -96,11 +96,13 @@ exports.updateCourse = asyncHandler(async (req, res, next) => {
 // @route   DELETE /api/v1/courses/:id
 // @access  Private
 exports.deleteCourse = asyncHandler(async (req, res, next) => {
-
   const course = await Course.findById(req.params.id);
 
   if (!course) {
-    return next(new ErrorResponse(`No course with the id of ${req.params.id}`), 404)
+    return next(
+      new ErrorResponse(`No course with the id of ${req.params.id}`),
+      404
+    );
   }
 
   await course.remove();
